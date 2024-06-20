db6a4a1fc30f02d7b3b3ba6d0fbe521309370a94
fayinana <fekedeananiya@gmail.com>
2024-06-20 22:28:51 +0300

    add three section with responsive lay out

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Manrope: ["Manrope", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
